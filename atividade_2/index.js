5da9e584ab2c03bcc4caa52bdfa0e4020b104c73
raphahf6 <raphaellima6@gmail.com>
2020-06-19 19:45:13 -0300

    envia atividade 2

--- atividade_2/index.js
+++ atividade_2/index.js
@@ -1,5 +1,19 @@
 // ## Importe a biblioteca ##
+let ls = require("readline-sync")
 
 
 
 // ## Faça o código ##
+
+let question1 = ls.question('Você esta usando a mascara de prevençao contra o covide-19 quando sai de casa? Responda sim ou nao ')
+let question2 = ls.question('Esta lavando as maos frequentemente? Responda sim ou nao ')
+console.log(question1)
+console.log(question2)
+ if(question1 && question2 == 'sim'){
+     console.log('Parabens!!! Você esta no caminho certo contra a pandemia')
+
+ } else if (question1 || question2 == 'nao') {
+     console.log('Foi por pouco! Melhore seus habitos')
+ } else {
+     console.log('Que pena, você não esta se cuidando!')
+ }
