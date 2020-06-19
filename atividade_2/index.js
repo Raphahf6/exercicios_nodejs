c6d6551916d4b0c7ed9695f1838584e949f860a2
raphahf6 <raphaellima6@gmail.com>
2020-06-19 20:48:48 -0300

    deixando os  app um pouco mais atraente no console

--- atividade_2/index.js
+++ atividade_2/index.js
@@ -4,6 +4,9 @@ let ls = require("readline-sync")
 
 
 // ## Faça o código ##
+console.log('=====================================================')
+console.log('Bem vindo, vamos verificar se você esta combatendo o covid-19 do jeito certo ')
+console.log('=====================================================')
 
 let question1 = ls.question('Você esta usando a mascara de prevençao contra o covide-19 quando sai de casa? Responda sim ou nao ')
 let question2 = ls.question('Esta lavando as maos frequentemente? Responda sim ou nao ')
--- atividade_3/index.js
+++ atividade_3/index.js
@@ -4,7 +4,12 @@ let ls = require("readline-sync")
 
 
 // ## Faça o código ##
+console.log('=====================================================')
+
 console.log('Bem vindo, vamos calcular o tamanho do quadrado? ')
+
+console.log('=====================================================')
+
 let question = ls.question('por favor digite o tamanho do lado do quadrado')
 
 let areaQuadrado = question
