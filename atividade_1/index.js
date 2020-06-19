fc3a9674055e16940b5702ff14ad0cf3a77c7448
raphahf6 <raphaellima6@gmail.com>
2020-06-19 19:33:11 -0300

    adiciona atividade 1 resolução

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -1,5 +1,9 @@
-// ## Importe a biblioteca ##
+let ls = require("readline-sync")
 
+let numVoo = ls.question('Qual o numero do seu voo ?')
+let fileira = ls.question('Em qual fileira você deseja sentar?')
 
-
-// ## Faça o código ##
+console.log('=====================================================')
+console.log('Cartão de embarque para o voo de numero: ' +  numVoo)
+console.log('Você sentara na fileira ' + fileira)
+console.log('=====================================================')
